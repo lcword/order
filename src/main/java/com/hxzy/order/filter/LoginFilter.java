f22c94997f5de09d906fb29fa4e60e4864dd5df3
Administrator <Administrator@PC-20170119CWOM>
2017-06-08 22:35:40 +0800

    权限v2.4

--- src/main/java/com/hxzy/order/filter/LoginFilter.java
+++ src/main/java/com/hxzy/order/filter/LoginFilter.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.hxzy.order.model.Function;
 import com.hxzy.order.service.intf.AdminService;
 import com.hxzy.order.util.GetEntity;
-
+/*懒加载可以解决问题，但是会引发lazyInitalizationException*/
 public class LoginFilter implements Filter {
 	private String noURI;
 	private AdminService adminService = GetEntity.getEntity("adminService");
@@ -30,17 +30,18 @@ public class LoginFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		Object user = req.getSession().getAttribute("login_user");
-		String uri = req.getRequestURI();
+		String url = req.getRequestURI();
+		System.out.println("url: " + url);
 		if (noURI != null && !"".equals(noURI)) {
 			String[] noURIArray = noURI.split(";");
 			for (String string : noURIArray) {
-				if (uri.endsWith(string) && jurisdiction(req, chain)) {
+				if (url.endsWith(string)) {
 					chain.doFilter(request, response);
 					return;
 				}
 			}
 		}
-		if (user != null && jurisdiction(req, chain)) {
+		if (user != null && jurisdiction(req, chain, user, url)) {
 			chain.doFilter(request, response);
 		} else {
 			HttpServletResponse resp = (HttpServletResponse) response;
@@ -53,19 +54,24 @@ public class LoginFilter implements Filter {
 		noURI = fConfig.getInitParameter("noURI");
 	}
 
-	private boolean jurisdiction(HttpServletRequest req, FilterChain chain) {
+	private boolean jurisdiction(HttpServletRequest req, FilterChain chain, Object loginer,String url) {
+		System.out.println(2222);
 		// 权限管理
-		String id = req.getSession().getAttribute("login_user").toString();
-		System.out.println("login_user_id: " + id);
-		if (id != null && !id.isEmpty()) {
-			String url = req.getRequestURL().toString();
-			System.out.println("adminService: " + adminService);
-			System.out.println("adminService.queryById(id): " + adminService.queryById(id));
-			System.out.println("adminService.queryById(id).getRole(): " + adminService.queryById(id).getRole());
-			Set<Function> set = adminService.queryById(id).getRole().getSet();
-			for (Function function : set) {
-				if (url.endsWith(function.getCode())) {
-					return true;
+		if(loginer != null){
+			System.out.println(3333);
+			String id = loginer.toString();
+			System.out.println("login_user_id: " + id);
+			if (id != null && !id.isEmpty()) {
+				System.out.println(4444);
+				System.out.println("adminService: " + adminService);
+				System.out.println("adminService.queryById(id): " + adminService.queryById(id));
+				System.out.println("adminService.queryById(id).getRole(): " + adminService.queryById(id).getRole());
+				Set<Function> set = adminService.queryById(id).getRole().getSet();
+				for (Function function : set) {
+					System.out.println(function.getCode());
+					if (url.endsWith(function.getCode())) {
+						return true;
+					}
 				}
 			}
 		}
